cfb31e436afd6d740a020c0e89e07a533122afc5
dganai <dinaganai@gmail.com>
2022-02-14 20:09:28 +0000

    closes #19; completion of updateMap function

--- routes/api/updateMap.js
+++ routes/api/updateMap.js
@@ -1,20 +1,20 @@
-module.exports = function (router, db) {
+module.exports = function(router, db) {
   const helper = require('./helpers')(db);
 
   //  update maps
   router.patch('/maps/:id', (req, res) => {
-   let updateQuery = `UPDATE maps SET `
+    let updateQuery = `UPDATE maps SET `;
     const parameters = [];
 
-   for (const key in req.body) {
-     parameters.push(req.body[key])
-     updateQuery += `
-      ${key} = $${parameters.length},`
-   }
-   updateQuery = updateQuery.slice(updateQuery.length - 1, 1, '')
-   parameters.push(req.params.id)
-   updateQuery += ` WHERE maps.id = $${parameters.length}`;
-   console.log(req.body);
-    helper.tryReturnJson(res, updateQuery, parameters)
+    for (const key in req.body) {
+      parameters.push(req.body[key]);
+      updateQuery += `
+      ${key} = $${parameters.length},`;
+    }
+    updateQuery = updateQuery.slice(0, -1);
+    parameters.push(req.params.id);
+    updateQuery += ` WHERE maps.id = $${parameters.length}`;
+
+    helper.tryReturnJson(res, updateQuery, parameters);
   });
-}
+};
