c46d3cf3c5449371c85b07b7fe571b9018388643
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-14 11:30:31 -0500

    closes #10; made user authentication url
    for dev purposes

--- routes/user/login.js
+++ routes/user/login.js
@@ -2,7 +2,7 @@ module.exports = function(router, db) {
   // GET /users/login
   router.get('/login/:id', (req, res) => {
     // cookie credentials
-    //req.session.user_id = req.params.id;
+    req.session.user_id = req.params.id;
 
     // redirect to homepage
     res.redirect('/');
