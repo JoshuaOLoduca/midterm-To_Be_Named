a12e4c182ce3aa97db00a5f9a1ae7ade8b9b7a9b
Armin Glavovic <glavovic.armin@gmail.com>
2022-02-14 17:26:33 +0000

    add getMapsId route

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,11 +1,12 @@
 const homePage = require('./homePage');
 const login = require('./login');
 const logout = require('./logout');
+const getMapsId = require('./getMapsId')
 module.exports = function (router, db) {
 
   //  render index page
   homePage(router, db);
-
+  getMapsId(router, db)
   // GET /users/login
   login(router, db);
 
