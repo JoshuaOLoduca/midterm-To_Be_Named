09062628e69bba7795f14d40eebc7fa739b2c371
dganai <dinaganai@gmail.com>
2022-02-14 19:33:47 +0000

    incomplete updatemap function

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,12 +4,14 @@ const getMaps = require('./getMaps');
 const userCollaborateAccess = require('./user/collaborateAccess');
 const userFavourites = require('./user/favourites');
 const userMaps = require('./user/maps');
+const updateMap = require('./updateMap');
 
 module.exports = function (router, db) {
 
   deleteMap(router, db);
   deletePlace(router, db);
   getMaps(router, db);
+  updateMap(router, db);
 
   // User stuff
   userCollaborateAccess(router, db);
---
+++ routes/api/updateMap.js
@@ -0,0 +1,20 @@
+module.exports = function (router, db) {
+  const helper = require('./helpers')(db);
+
+  //  update maps
+  router.patch('/maps/:id', (req, res) => {
+   let updateQuery = `UPDATE maps SET `
+    const parameters = [];
+
+   for (const key in req.body) {
+     parameters.push(req.body[key])
+     updateQuery += `
+      ${key} = $${parameters.length},`
+   }
+   updateQuery = updateQuery.slice(updateQuery.length - 1, 1, '')
+   parameters.push(req.params.id)
+   updateQuery += ` WHERE maps.id = $${parameters.length}`;
+   console.log(req.body);
+    helper.tryReturnJson(res, updateQuery, parameters)
+  });
+}
