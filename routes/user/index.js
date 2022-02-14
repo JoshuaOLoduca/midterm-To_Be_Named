14d0b0499f093dac13be497696d0d0a6a4be01e1
dganai <dinaganai@gmail.com>
2022-02-14 15:57:04 +0000

    Link for createMaps route

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,6 +1,7 @@
 const homePage = require('./homePage');
 const login = require('./login');
 const logout = require('./logout');
+const createMaps = require('./createMaps');
 module.exports = function (router, db) {
 
   //  render index page
@@ -12,6 +13,8 @@ module.exports = function (router, db) {
   // POST /users/logout
   logout(router, db);
 
+  // POST /maps/create
+  createMaps(router, db);
   return router;
 }
 
