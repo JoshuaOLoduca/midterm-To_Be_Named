2dfb96fd759b92854951f29ba563725021791c85
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-14 20:11:07 -0500

    closes #29, closes #18; user can see and load all maps that are public

---
+++ public/scripts/userRelMaps/app.js
@@ -0,0 +1,65 @@
+const allMaps = [];
+
+const $mapContainer = $('#mapContainer');
+
+let ajaxUrl = '';
+
+switch(renderType) {
+  case 'allMaps':
+    ajaxUrl = '/api/maps';
+    break;
+  case 'favourites':
+    ajaxUrl = `/api/users/${userId}/maps/favourites`;
+    break;
+  case 'collaborator':
+    ajaxUrl = `/api/users/${userId}/maps/collaborate`;
+    break;
+}
+
+$.ajax({
+  method: "GET",
+  url: ajaxUrl
+})
+.done(function( msg ) {
+  msg.forEach(element => {
+    allMaps.unshift(element);
+  });
+
+  renderMaps();
+});
+
+function renderMaps(howManyToShowPerRender = 10) {
+  if (!allMaps.length) return;
+
+  for (let i = 0; i < howManyToShowPerRender; i++) {
+    renderMapToScreen(allMaps.pop());
+  }
+}
+
+// title:
+// city:
+// cover_img:
+// description:
+
+// id:
+// owner_id:
+// public:
+
+function renderMapToScreen(mapData) {
+  const $element = $(`
+    <h2>${mapData.title}</h2>
+    <h4>${mapData.city}</h4>
+    <p>${mapData.description}</p>
+    <img src='${mapData.cover_img}'/>
+  `);
+
+  $element.on('click', e => {
+    window.location.assign("/maps/"+mapData.id);
+  })
+
+  $mapContainer.append($element);
+}
+
+$('#loadMoar').on('click', e => {
+  renderMaps();
+})
--- routes/main/getMapsId.js
+++ routes/main/getMapsId.js
@@ -4,16 +4,29 @@ module.exports = function (router, db) {
 
     const id = req.params.id
 
-    const queryString = `
-    SELECT maps.id
+    const mapQueryString = `
+    SELECT *
     FROM maps
     WHERE id = $1;
     `;
 
-    db.query(queryString, [id])
+    const mapPlacesQueryString = `
+    SELECT p.*
+    FROM places p
+    WHERE map_id = $1;
+    `;
+
+    db.query(mapQueryString, [id])
       .then((response) => {
-        const result = response.rows[0]
-        if (result) res.render('map');
+        const mapDetails = response.rows[0]
+
+        db.query(mapPlacesQueryString, [mapDetails.id])
+        .then(moreResponse => {
+          const moarResult = moreResponse.rows;
+          if (!mapDetails) throw new Error('uhoh');
+
+          res.render('map', {map: mapDetails, places: moarResult});
+        })
       })
       .catch((err) => { if (err) {
         res.status(404).send('wrong stuff');
