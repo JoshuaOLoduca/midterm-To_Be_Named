c4a0ae5eae6de6161c4c7fa3bcbf2cc8a7d9b54a
Armin Glavovic <glavovic.armin@gmail.com>
2022-02-15 23:35:54 +0000

    add markers to map as places

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,25 +11,30 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiZ2xhdm92aWNhIiwiYSI6ImNrem9hMjFuYzBkd3EydW13OGU5bzlid3kifQ.C_IQD6mnpvTVu5GosTyrJQ'
 }).addTo(map);
 
-var marker = L.marker([45.434895, -73.683872]).addTo(map);
+console.log(places)
+
+for (const place of places) {
+  L.marker([place.latitude, place.longitude]).addTo(map)
+}
+// var marker = L.marker([45.434895, -73.683872]).addTo(map);
 
 
-var circle = L.circle([51.508, -0.11], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 500
-}).addTo(map);
+// var circle = L.circle([51.508, -0.11], {
+//   color: 'red',
+//   fillColor: '#f03',
+//   fillOpacity: 0.5,
+//   radius: 500
+// }).addTo(map);
 
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(map);
+// var polygon = L.polygon([
+//   [51.509, -0.08],
+//   [51.503, -0.06],
+//   [51.51, -0.047]
+// ]).addTo(map);
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");
 
 
 function onMapClick(e) {
