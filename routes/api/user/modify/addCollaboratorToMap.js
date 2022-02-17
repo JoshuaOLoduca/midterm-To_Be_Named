6296e92aa9e273e2698f001567fd09b71d51dea2
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-17 18:45:25 -0500

    adding owner check to adding colaborator

--- routes/api/user/modify/addCollaboratorToMap.js
+++ routes/api/user/modify/addCollaboratorToMap.js
@@ -3,36 +3,53 @@ module.exports = function(router, db){
 
   router.post('/maps/:id/collaborators', (req, res) => {
 
-    // to be used to verify editing rights
     const user_id = req.session.user_id;
+    const map_id = req.body.map_id;
+    const userToAdd = req.body.id;
 
-    const insertString = `
-    INSERT INTO collaborators (user_id, map_id)
-    VALUES ($1, $2)
-    RETURNING *;
+    const checkRightsQuery = `
+    SELECT m.owner_id
+    FROM maps m
+    WHERE m.owner_id = $1 AND m.id = $2;
     `;
-    const params = [
-      req.body.id,
-      req.body.map_id
-    ]
 
-    db.query(insertString, params)
-    .then(result => {
-      const queryString = `
-      SELECT user_id, name, map_id
-      FROM collaborators c
-      JOIN users u ON u.id = c.user_id
-      WHERE map_id = $2 AND u.id = $1
-      GROUP BY user_id, name, map_id;
+    db.query(checkRightsQuery, [user_id, map_id])
+    .then(response => {
+      const result = response.rows;
+      if (!result.length) return res.status(403).send('You dont have rights')
+      addCollaboratorToMap();
+    })
+
+    function addCollaboratorToMap() {
+      const insertString = `
+      INSERT INTO collaborators (user_id, map_id)
+      VALUES ($1, $2)
+      RETURNING *;
       `;
-
-      const moarParams = [
-        result.rows[0].user_id,
-        req.body.map_id,
+      const params = [
+        userToAdd,
+        map_id
       ]
 
-      helper.tryReturnJson(res, queryString, moarParams)
-    })
-    .catch(e => console.log(e))
+      db.query(insertString, params)
+      .then(result => {
+        const queryString = `
+        SELECT user_id, name, map_id
+        FROM collaborators c
+        JOIN users u ON u.id = c.user_id
+        WHERE map_id = $2 AND u.id = $1
+        GROUP BY user_id, name, map_id;
+        `;
+
+        const moarParams = [
+          result.rows[0].user_id,
+          req.body.map_id,
+        ]
+
+        helper.tryReturnJson(res, queryString, moarParams)
+      })
+      .catch(e => console.log(e))
+    }
+
   });
 }
