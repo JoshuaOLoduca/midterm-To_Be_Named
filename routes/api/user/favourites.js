60e79875477685e7c1a76040dbe5b8a301c3f5b7
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 21:05:01 -0500

    commenting and cleaning getUserFacourites Route

--- routes/api/user/favourites.js
+++ routes/api/user/favourites.js
@@ -1,18 +1,22 @@
-module.exports = function (router, db) {
+module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require('../helpers')(db);
 
+  // GET /api/users/{user id}/maps/favourites
+  // Get all favourite maps of user
   router.get('/users/:user_id/maps/favourites', (req, res) => {
 
-    const {user_id} = req.params;
+    // Get user id from url
+    const user_id = req.params.user_id;
 
     const queryString = `
-    SELECT m.*
-    FROM maps m
-    JOIN user_favourites ul ON m.id = ul.map_id
-    WHERE ul.user_id = $1;
-    `;
+      SELECT m.*
+      FROM maps m
+      JOIN user_favourites ul ON m.id = ul.map_id
+      WHERE ul.user_id = $1;
+      `;
 
+    // Query database for favourite maps of user and returns array to of maps to client
     helper.tryReturnJson(res, queryString, [ user_id ]);
   });
-}
+};
