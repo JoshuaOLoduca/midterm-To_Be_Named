05f33750ea21c1bee7ea4dd4f9eb26c41bbcaf9b
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 20:49:21 -0500

    cleaning and commenting updateMap Route

--- routes/api/user/modify/updateMap.js
+++ routes/api/user/modify/updateMap.js
@@ -2,33 +2,58 @@ module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require('../../helpers')(db);
 
-  //  update maps
+  // PATCH /api/maps/{id}
+  // Takes in body of new map details
+  // Updates existing map with new details
   router.patch('/maps/:id', (req, res) => {
-    if (!req.body) res.status(400).send('No info in body');
+    // If nothing in body, exit function and let user know they need to send in data
+    if (!req.body) return res.status(400).send('No info in body');
 
+    // Get logged in user
     const user_id = req.session.user_id;
+    // Get map id from url
     const map_id = req.params.id;
 
-    let updateQuery = `UPDATE maps SET `;
-    const parameters = [];
 
+    // Verifies logged in user owns the map
+    // Helper will let  user know if user doesnt have perms
     helper.checkIfOwner(res, user_id, map_id)
-    .then(() => updateMap())
-    .catch(err => {
-      console.log(err);
-      res.status(500).send('Something went wrong on our end')
-    });
+      // On success, delete collaborator
+      .then(() => updateMap())
+      // General Error Catcher
+      .catch(err => {
+      // Log error to server console
+        console.log(err);
+        // Let user know the server oopsied
+        res.status(500).send('Something went wrong on our end');
+      });
 
     function updateMap() {
+      // Initialize sql query paramater holder
+      const parameters = [];
+      // Start sql query
+      let updateQuery = `UPDATE maps SET `;
+
+      // Loop though body for map data
+      // Set key as column name, and value as the new column value
       for (const key in req.body) {
+        // Push new column value to params
         parameters.push(req.body[key]);
+
+        // Add it to query string
         updateQuery += `
         ${key} = $${parameters.length},`;
       }
+
+      // Remove trailing comma
       updateQuery = updateQuery.slice(0, -1);
-      parameters.push(req.params.id);
+
+      // Push map id to to parameters
+      parameters.push(map_id);
+      // Append where clause to sql query
       updateQuery += ` WHERE maps.id = $${parameters.length}`;
 
+      // Send update to database and return newly updated values
       helper.tryReturnJson(res, updateQuery, parameters);
     }
   });
