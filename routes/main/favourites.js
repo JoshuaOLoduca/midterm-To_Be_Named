bb845a7f85a68986d706a6be0d3993c0dd1bdf93
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 21:45:43 -0500

    commenting and cleaning favourite view route

--- routes/main/favourites.js
+++ routes/main/favourites.js
@@ -1,22 +1,24 @@
 module.exports = function(router, db) {
-  // GET /users/login
+  // GET /users/favourites
+  // Renders page of maps user has favourited
   router.get('/users/favourites', (req, res) => {
-    // cookie credentials
+    // Get logged in user
     const userId = req.session.user_id;
 
-    if (!userId) return res.send('Login nerd')
+    // If they arent logged in, tell them to
+    if (!userId) return res.send('Login nerd');
 
+    // Assemble EJS vars
     const templateVars = {
       renderType: 'favourites',
       userId
-    }
+    };
 
-    console.log(templateVars)
 
-    // redirect to homepage
+    // Render and show the logged in users favourite maps
     res.render('userReleventMaps', templateVars);
   });
 
   // redirect to proper spelling
   router.get('/users/favorites', (req, res) => res.redirect('/users/favourites'));
-}
+};
