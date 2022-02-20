4ff6329ae5c7b291b0f15d6ab9cde6de3ac3ec4d
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 21:18:42 -0500

    commenting and cleaning api route for getMaps

--- routes/api/getMaps.js
+++ routes/api/getMaps.js
@@ -1,38 +1,47 @@
-module.exports = function (router, db) {
+module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require('./helpers')(db);
 
+  // GET /api/maps
+  // Gets all public maps
   router.get('/maps', (req, res) => {
     const queryString = `
-    SELECT *
-    FROM maps
-    WHERE public = true;
-    `;
+      SELECT *
+      FROM maps
+      WHERE public = true;
+      `;
 
-    helper.tryReturnJson(res, queryString)
+    // Get all public maps and return list of maps to client
+    helper.tryReturnJson(res, queryString);
   });
 
+  // GET /api/maps/mostViewed
+  // Gets all public maps and sorts them by views
   router.get('/maps/mostViewed', (req, res) => {
     const queryString = `
-    SELECT *
-    FROM maps
-    WHERE public = true
-    ORDER BY views DESC;
-    `;
+      SELECT *
+      FROM maps
+      WHERE public = true
+      ORDER BY views DESC;
+      `;
 
-    helper.tryReturnJson(res, queryString)
+    // queries db and returns list of maps sorted by views
+    helper.tryReturnJson(res, queryString);
   });
 
+  // GET /api/maps/mostLiked
+  // Gets all public maps and sorts them by favourite count
   router.get('/maps/mostLiked', (req, res) => {
     const queryString = `
-    SELECT m.*, COUNT(uf) as likes
-    FROM maps m
-    JOIN user_favourites uf ON uf.map_id = m.id
-    WHERE public = true
-    GROUP BY m.id
-    ORDER BY COUNT(uf) DESC;
-    `;
+      SELECT m.*, COUNT(uf) as likes
+      FROM maps m
+      JOIN user_favourites uf ON uf.map_id = m.id
+      WHERE public = true
+      GROUP BY m.id
+      ORDER BY COUNT(uf) DESC;
+      `;
 
-    helper.tryReturnJson(res, queryString)
+    // queries db and returns list of maps sorted by favourites
+    helper.tryReturnJson(res, queryString);
   });
-}
+};
