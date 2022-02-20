feabefeb790b24e1554dc9e076666123bc6a0f25
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 21:12:31 -0500

    commenting and cleaning users maps route

--- routes/api/user/maps.js
+++ routes/api/user/maps.js
@@ -1,18 +1,22 @@
-module.exports = function (router, db) {
+module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require('../helpers')(db);
 
+  // GET /api/users/{user id}/maps
+  // Gets maps made by user
   router.get('/users/:user_id/maps', (req, res) => {
 
+    // Get user id from url
     const {user_id} = req.params;
 
     const queryString = `
-    SELECT *
-    FROM maps m
-    WHERE m.owner_id = $1
-    ORDER BY m.id;
-    `;
+      SELECT *
+      FROM maps m
+      WHERE m.owner_id = $1
+      ORDER BY m.id;
+      `;
 
+    // Returns list of maps made by user
     helper.tryReturnJson(res, queryString, [ user_id ]);
   });
-}
+};
