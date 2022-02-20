84b49dc8e2d37e82fd6df9b09bceb8b813d5b3fc
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 22:02:08 -0500

    commenting and cleaning allMaps route

--- routes/main/maps.js
+++ routes/main/maps.js
@@ -1,15 +1,19 @@
-const { Template } = require("ejs");
+module.exports = function(router, db) {
 
-module.exports = function (router, db) {
-
+  // GET /maps
+  // Get list of all publicly available maps and renders the page for user
   router.get('/maps', (req, res) => {
 
-    const userId = req.session.user_id
+    // Gets Logged in user
+    const userId = req.session.user_id;
 
+    // Passes vars to EJS
     const templatevars = {
       renderType: 'allMaps',
       userId
-    }
-    res.render("userReleventMaps", templatevars)
+    };
+
+    // Renders all maps and sends it to user
+    res.render("userReleventMaps", templatevars);
   });
 };
