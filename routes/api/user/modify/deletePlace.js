55a52b1e3fa4f06bbfdb4c8cacb06c5a6717659d
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 20:34:02 -0500

    commenting and cleaning deletePlace route

--- routes/api/user/modify/deletePlace.js
+++ routes/api/user/modify/deletePlace.js
@@ -1,11 +1,14 @@
-module.exports = function (router, db) {
+module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require('../../helpers')(db);
 
-  //  render index page
+  // DELETE /api/places/{id}
+  // Takes in no params and deletes place from map if user has rights
   router.delete('/places/:id', (req, res) => {
-    const place_id = req.params.id
+    // Get Logged in user
     const user_id = req.session.user_id;
+    // Get place if from url
+    const place_id = req.params.id;
 
     const checkRightsQuery = `
       SELECT m.owner_id AS user_id, m.id AS map_id
@@ -19,22 +22,29 @@ module.exports = function (router, db) {
       JOIN collaborators c ON c.map_id = m.id
       WHERE p.id = $2 AND c.user_id = $1;
     `;
+
+    // Check if user has rights for place
+    // helper will let user know if they dont have perms
     helper.checkRights(res, checkRightsQuery, [user_id, place_id])
-    .then(() => deletePlace())
-    .catch(err => {
-      console.log(err);
-      res.status(500).send('Something went wrong on our end')
-    });
+      // If they do, delete it
+      .then(() => deletePlace())
+      // General Error Catcher
+      .catch(err => {
+        // Log error to server console
+        console.log(err);
+        // Let user know the server oopsied
+        res.status(500).send('Something went wrong on our end');
+      });
 
     function deletePlace() {
       const deleteString = `
-      DELETE
-      FROM places p
-      WHERE p.id = $1
+        DELETE
+        FROM places p
+        WHERE p.id = $1
         RETURNING *;
         `;
 
-        helper.tryDeleteEntity(res, deleteString, [place_id])
-      }
+      helper.tryDeleteEntity(res, deleteString, [place_id]);
+    }
   });
-}
+};
