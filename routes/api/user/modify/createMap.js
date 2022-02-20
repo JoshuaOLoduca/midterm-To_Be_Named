c868556923d64c0eafaaac4798b782f39d7e2297
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 20:18:19 -0500

    commenting and cleaning createMap route

--- routes/api/user/modify/createMap.js
+++ routes/api/user/modify/createMap.js
@@ -2,17 +2,25 @@
 module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require("../../helpers")(db);
+
   // POST /maps/create
+  // Takes in body of map object and inserts into database under logged in user
   router.post('/maps', (req, res) => {
+    // Get logged in users id
     const user_id = req.session.user_id;
 
+    // Verify they exist in our records
+    // If they are not, helper class will let them know
     helper.checkIfUserExists(res, user_id)
-    .then(() => createMap())
-    .catch(err => {
-      console.log(err);
-      res.status(500).send('Something went wrong on our end')
-    });
+      // If they do, creat map in database
+      .then(() => createMap())
+      // General error catcher
+      .catch(err => {
+        console.log(err);
+        res.status(500).send('Something went wrong on our end');
+      });
 
+    // Inserts map into db and returns created map with its db assigned id
     function createMap() {
       const queryString = `
       INSERT INTO maps (owner_id, city, title, description, cover_img)
@@ -26,13 +34,16 @@ module.exports = function(router, db) {
         req.body.cover_img,
       ];
 
+      // Send insert to database
       db.query(queryString, values)
+        // If inserted, return map data as single object to client
         .then((result) => {
           res.status(200).json(result.rows[0]);
         })
+        // General error catcher
         .catch((err) => {
           console.log(err);
-          res.json({ error: err.message});
+          res.status(500).send('Something went wrong on our end');
         });
     }
   });
