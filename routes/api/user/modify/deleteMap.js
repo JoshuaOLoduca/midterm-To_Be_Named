dbe87125c126a5459bc93fbaa9e5f6b85f80261a
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-19 20:29:20 -0500

    commenting and cleaning mapDelete route

--- routes/api/user/modify/deleteMap.js
+++ routes/api/user/modify/deleteMap.js
@@ -1,20 +1,31 @@
-module.exports = function (router, db) {
+module.exports = function(router, db) {
   // Import helper and initialize it with the database connection
   const helper = require('../../helpers')(db);
 
-  //  render index page
+  // DELETE /api/maps{id}
+  // Takes in nothing, deletes map if logged in user owns it
   router.delete('/maps/:id', (req, res) => {
+    // Get logged in user
     const user_id = req.session.user_id;
-    const map_id = req.params.id
+    // Get map id from url
+    const map_id = req.params.id;
 
+    // Verify logged in user owns map
+    // Helper will let them know if they dont have perms
     helper.checkIfOwner(res, user_id, map_id)
-    .then(() => deleteMap())
-    .catch(err => {
-      console.log(err);
-      res.status(500).send('Something went wrong on our end')
-    });
+      // If they do, delete it
+      .then(() => deleteMap())
+      // General error catcher
+      .catch(err => {
+      // Log error to server console
+        console.log(err);
+        // Let user know the server oopsied
+        res.status(500).send('Something went wrong on our end');
+      });
 
+    // Delete map from db
     function deleteMap() {
+      // Add RETURNING so we can verify if there was anything deleted
       const deleteString = `
       DELETE
       FROM maps
@@ -23,7 +34,7 @@ module.exports = function (router, db) {
       RETURNING *;
       `;
 
-      helper.tryDeleteEntity(res, deleteString, [map_id, user_id])
+      helper.tryDeleteEntity(res, deleteString, [map_id, user_id]);
     }
   });
-}
+};
